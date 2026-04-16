a7adba690c9a39c7e213889af711d43c2256c6f0
Chandresh Sahu <sahuchandresh46@gmail.com>
2026-04-16 15:38:40 +0530

    fix session UI data binding

--- client/src/pages/admin/Session.jsx
+++ client/src/pages/admin/Session.jsx
@@ -74,7 +74,73 @@ const Session = () => {
 
     return (
         <div>
-            {/* UI same as before */}
+            <div>
+       <div className="row my-4">
+        <div className="col-sm-10 mx-auto">
+          <div className="card">
+           <div className="card-body">
+             <form method='POST' onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-sm-12">
+                <input type="text" name='name' value={form.name} placeholder='Enter name'  className='form-control' onChange={handleChange}/>
+              </div>
+              <div className="col-sm-12 my-3">
+                <textarea name="description" onChange={handleChange} placeholder='Enter Description'
+                value={form.description}
+                  className='form-control' id=""></textarea>
+              </div>
+              <div className="col-sm-6">
+                <input type="submit" className='btn btn-danger' />
+              </div>
+            </div>
+            </form>
+           </div>
+          </div>
+        </div>
+       </div>
+       <div className="row my-4">
+        <div className="col-sm-10 mx-auto">
+          <div className="card">
+            <div className="card-body">
+              <h2>Sessions</h2>
+              <table className='table table-bordered'>
+                <thead>
+                  <tr>
+                    <th>S.No</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Date</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Map through sessions data here */}
+                
+                  {data.map((item , i)=>(
+                    <tr key={item._id}>
+                      <td>{i+1}</td>
+                      <td>{item.name}</td>
+                      <td>{item.description}</td>
+                      <td>{item.createdAt}</td>
+                      <td>
+                        <button onClick={()=>{
+                          handleDelete(item._id)
+                        }}>Delete</button>
+
+                        <button onClick={()=>{
+                          handleEdit(item)
+                        }}>Edit</button>
+                      </td>
+                    </tr>
+                  ))}
+              
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+       </div>
+    </div>
         </div>
     )
 }
